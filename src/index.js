89d17bdc2719047508921e44e77d346860167c1e
nekootoko3 <miyako.kerria@gmail.com>
2022-04-05 01:51:02 +0900

    Step 4: fiber

--- src/index.js
+++ src/index.js
@@ -21,25 +21,96 @@ function createTextElement(text) {
 }
 
 function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+
+  // 子要素を再帰的にノードに追加
+  //element.props.children.forEach((child) => render(child, dom));
+
+  // 作成したノードを container に追加
+  //container.appendChild(dom);
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+// メインスレッドがアイドル状態のときにブラウザがコールバックを実行
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // もし追加されていなければ、自身の dom を fiber から作成し親ノードに追加
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // 子要素ごとに新しいファイバーを作成
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // 作業単位を検索。 最初に子要素、次に兄弟、次におじというように試みる
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
+
+function createDom(fiber) {
   // element type に応じてノードを作成
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   // children を除く property の割り当て
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  // 子要素を再帰的にノードに追加
-  element.props.children.forEach((child) => render(child, dom));
-
-  // 作成したノードを container に追加
-  container.appendChild(dom);
+  return dom;
 }
 
 const Didact = {
